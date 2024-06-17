ef0897d4a92e871d16c564cfd5d89d806ab3d6b6
Chi <azarashibobobo@gmail.com>
2024-06-17 17:04:56 +0100

    updated header

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -7,12 +7,15 @@ export default function Header() {
   return (
     <header className={styles.header}>
       <div className={styles.container}>
+        <div className={styles.logoContainer2}>
         <div className={styles.logoContainer}>
           <Link to="/" className={styles.logo}> <span className={styles.title}>FutureTech</span></Link>
         </div>
-        <MainNav />
+        </div>
+      <MainNav />
        {/* Could put searchbar here?? Something doesn't look right though */}
       </div>
+      
     </header>
   );
 }

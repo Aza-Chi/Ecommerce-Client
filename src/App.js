a8e02bf5120fbdce4ebd1a07969ecb9eb40d1d20
Chi <azarashibobobo@gmail.com>
2024-06-19 23:46:22 +0100

    updated App.js

--- src/App.js
+++ src/App.js
@@ -1,21 +1,62 @@
 import { Outlet } from "react-router-dom";
 import Header from './components/Header/Header';
+import axios from 'axios';
 
+//Fetch authLoader 
+// export async function authLoader() {
+//   // https://reactrouter.com/en/main/start/tutorial#loading-data
+//   // https://reactrouter.com/en/main/route/loader
+//   try {
+//     console.log(`Fetching auth/status`)
+//     const res = await fetch(
+//       `${process.env.REACT_APP_API_BASE_URL}/auth/status`,
+//       { credentials: "include" }
+//     );
+//     //testing
+//     console.log(res);
+//     //Need to set logged in to true ! 
+//     //testing end 
+//     if (res.ok) {
+//       const authData = await res.json();
+//       console.log("authData: ");
+//       console.log(authData);
+//       return authData;
+//     }
+//     throw new Error("Unexpected status code.");
+//   } catch (error) {
+//     return { logged_in: false, id: null, email_address: null, auth_method: null };
+//   }
+// }
+
+//Axio authLoader 
 
 export async function authLoader() {
-  // https://reactrouter.com/en/main/start/tutorial#loading-data
-  // https://reactrouter.com/en/main/route/loader
   try {
-    const res = await fetch(
-      `${process.env.REACT_APP_API_BASE_URL}/auth/status`,
-      { credentials: "include" }
-    );
-    if (res.ok) {
-      const authData = await res.json();
+    
+    console.log(`App.js - Fetching auth/status`);
+    const token = localStorage.getItem('token');
+    const res = await axios.get(`${process.env.REACT_APP_API_BASE_URL}/auth/status`, {
+      headers: {
+        'Authorization': `${token}`, // Include JWT token as Bearer token
+      },
+      //withCredentials: true, // If needed for CORS with credentials
+    });
+    
+    // Testing
+    console.log(`App.js - res.data results printed as authData:`);
+    //console.log(res); // returns object with data: auth_method: null, email_address: null, id: null, logged_in: false, 
+    // Testing end 
+
+    if (res.status === 200) {
+      const authData = res.data;
+      console.log('authData:', authData);
+      
       return authData;
     }
+    
     throw new Error("Unexpected status code.");
   } catch (error) {
+    console.error(error);
     return { logged_in: false, id: null, email_address: null, auth_method: null };
   }
 }
